613db473f903c571a82d16de36cca421bd296985
AbyChan <crystal.tyan@gmail.com>
2016-03-19 00:17:14 +0800

    themes

--- themes/white-card/static/js/article.js
+++ themes/white-card/static/js/article.js
@@ -1,7 +1,10 @@
 
 
-(function(){
+(function(window, $){
     
+    $('img').wrap('<div class="img-wrapper">');
+    $('.img-wrapper').each(function(i, e){
+        $(e).append('<span class="line"></span>')
+    });
     
-    
-})();
+})(window, jQuery);
