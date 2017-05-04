2602c9ac49633ad64dbf739ee6356f7fcdb5cd78
fwchen <crystal.tyan@gmail.com>
2017-05-04 14:45:33 +0800

    initial commit

--- command/render.js
+++ command/render.js
@@ -4,6 +4,7 @@ import {loadConfig, readConfigure} from '../lib/loadConfig';
 import {output, error, log} from '../lib/message';
 import {isFile, isDir, logCurrentTime, injectGlobal, filterDotFiles} from '../lib/util';
 import {renderFile, RenderController, renderDir, writeFile, joinPwd} from '../lib/render';
+import ora from 'ora';
 
 import fs from 'fs';
 import path from 'path';
@@ -16,19 +17,19 @@ export default class RenderCommand {
   getName() {
     return this.name;
   }
-  
+
   async run(inputs, flags) {
     const inputPath = inputs[0];
     if( isDir(inputPath) ) {
       let outputPath = outputPath || 'build';
 
-      let tickProcess = new TickProcess();
-      tickProcess.start();
 
-      let options = readConfigure(inputPath);
+      const spinner = ora('Start render...').start();
+      const options = readConfigure(inputPath);
       const renderControl = new RenderController(inputPath, outputPath, options);
       await renderControl.render();
-      tickProcess.stop();
+
+      spinner.succeed('Build success...');
       logCurrentTime();
       log('Render completion!');
     } else {
