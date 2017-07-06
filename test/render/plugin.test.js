c68ed7b1772672a6c81b36b9776aa4baea4d6302
fwchen <crystal.tyan@gmail.com>
2017-07-06 16:09:40 +0800

    test

---
+++ test/render/plugin.test.js
@@ -0,0 +1,14 @@
+import RenderPlugin from '../../modules/render/render-plugin';
+
+let renderPlugin;
+
+beforeEach(() => {
+  renderPlugin = new RenderPlugin();
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  const pluginNames = Object.values(renderPlugin.getPlugin()).map(p =>
+    p.getName()
+  );
+  expect(pluginNames).toEqual(['ajax']);
+});
