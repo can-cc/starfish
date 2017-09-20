ccf2077704c68fcdd183e4c64c2d8b10b7110155
fwchen <crystal.tyan@gmail.com>
2017-09-20 11:51:43 +0800

    remove unused imported

--- src/modules/render/render-plugin.js
+++ src/modules/render/render-plugin.js
@@ -1,31 +1,21 @@
-import bluebird from 'bluebird';
-import fs from 'fs';
-import path from 'path';
-import _ from 'lodash';
-import ejs from 'ejs';
-import fsExtra from 'fs-extra';
-import moment from 'moment';
-import md5 from 'blueimp-md5';
 import R from 'fw-ramda';
-
-import { parseOrg, parseMarkDown } from './render-parse.js';
-import {
-  isFile,
-  isDir,
-  takeFileName,
-  takeFileNameWithoutSuffix,
-  getRelativePath,
-  filterDotFiles,
-  isSuffix,
-  mergeForce
-} from '../../lib/util';
-import { warning, error } from '../../lib/message';
-import { loadConfig } from '../../lib/loadConfig.js';
-import { getModifyDates } from '../../util/git-date';
 
 export class RenderPluginManager {
   constructor(options) {
     this.plugins = this.getPluginFromNodeMudules(options);
+  }
+
+  getPluginFromNodeMudules(options) {
+    const plugins = {};
+    ['nobbb-render-ajax'].forEach(name => {
+      if (!plugins[name]) {
+        const plugin = new (require(name)).default(options);
+        plugins[plugin.getName()] = plugin;
+      } else {
+        throw new Error('duplicate plugin');
+      }
+    });
+    return plugins;
   }
 
   getPlugin() {
@@ -62,18 +52,7 @@ export class RenderPluginManager {
     R.values(this.plugins).forEach(plugin => plugin.afterwCategoryRender(rendered, data));
   }
 
-  getPluginFromNodeMudules(options) {
-    const plugins = {};
-    ['nobbb-render-ajax'].forEach(name => {
-      if (!plugins[name]) {
-        const plugin = new (require(name)).default(options);
-        plugins[plugin.getName()] = plugin;
-      } else {
-        throw new Error('duplicate plugin');
-      }
-    });
-    return plugins;
-  }
+
 }
 
 export default RenderPluginManager;
