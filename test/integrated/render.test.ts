ba35e6c4ca23464b1832a51fb0f5bc4cb044646d
fwchen <crystal.tyan@gmail.com>
2018-06-05 13:04:22 +0800

    fix test

--- test/integrated/render.test.ts
+++ test/integrated/render.test.ts
@@ -2,13 +2,14 @@ import { RenderController } from '../../src/modules/render/render-controller';
 import * as fs from 'fs';
 import * as path from 'path';
 import { execSync } from 'child_process';
+import { readConfigure } from '../../src/lib/loadConfig';
 
 const inputPath = 'test/mock-source/';
 const outputPath = 'test/test-build/';
 const outputPathAbsolutelyPath = path.resolve(__dirname, '../../', outputPath);
 
 test('integrated test render feather', async () => {
-  const renderCtrl = new RenderController(inputPath, outputPath);
+  const renderCtrl = new RenderController(inputPath, outputPath, readConfigure(path.join(__dirname, '../mock-source')));
   await renderCtrl.render();
 
   expect(fs.existsSync(path.join(outputPath, 'index.html'))).toBe(true);
