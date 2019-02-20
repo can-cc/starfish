224cc1a2f3d3b9a6dd60ff665eed82896ec8be2d
fwchen <me@chenfangwei.com>
2019-02-20 10:21:17 +0800

    split render test expect

--- test/integrated/render.test.ts
+++ test/integrated/render.test.ts
@@ -1,22 +1,31 @@
 import { RenderController } from '../../src/modules/render/render-controller';
 import * as fs from 'fs';
 import * as path from 'path';
-import { execSync } from 'child_process';
 import { readConfigure } from '../../src/lib/loadConfig';
 
 const inputPath = 'test/mock-source/';
 const outputPath = 'test/test-build/';
 const outputPathAbsolutelyPath = path.resolve(__dirname, '../../', outputPath);
 
-test('integrated test render feather', async () => {
+test('integrated test render index.html', async () => {
   const renderCtrl = new RenderController(inputPath, outputPath, readConfigure(path.join(__dirname, '../mock-source')));
   await renderCtrl.render();
 
   expect(fs.existsSync(path.join(outputPath, 'index.html'))).toBe(true);
+});
+
+test('integrated test render category index', async () => {
+  const renderCtrl = new RenderController(inputPath, outputPath, readConfigure(path.join(__dirname, '../mock-source')));
+  await renderCtrl.render();
+
   expect(fs.existsSync(path.join(outputPath, 'categorys/index.html'))).toBe(true);
   expect(fs.existsSync(path.join(outputPath, 'categorys/index.json'))).toBe(true);
-  expect(fs.existsSync(path.join(outputPath, 'javascript/hello-word/index.html'))).toBe(true);
-  expect(fs.existsSync(path.join(outputPath, 'javascript/hello-word/index.json'))).toBe(true);
+});
+
+test('integrated test render feather', async () => {
+  const renderCtrl = new RenderController(inputPath, outputPath, readConfigure(path.join(__dirname, '../mock-source')));
+  await renderCtrl.render();
+
   expect(fs.existsSync(path.join(outputPath, 'javascript/index.html'))).toBe(true);
   expect(fs.existsSync(path.join(outputPath, 'javascript/index.json'))).toBe(true);
   expect(fs.existsSync(path.join(outputPath, 'javascript/metor/index.json'))).toBe(true);
@@ -24,8 +33,23 @@ test('integrated test render feather', async () => {
   expect(fs.existsSync(path.join(outputPath, 'javascript/metor/test.png'))).toBe(true);
   expect(fs.existsSync(path.join(outputPath, 'favicon.ico'))).toBe(true);
   expect(fs.existsSync(path.join(outputPath, 'sitemap.txt'))).toBe(true);
+});
+
+test('integrated test render javascript/hello-word', async () => {
+  const renderCtrl = new RenderController(inputPath, outputPath, readConfigure(path.join(__dirname, '../mock-source')));
+  await renderCtrl.render();
+
+  expect(fs.existsSync(path.join(outputPath, 'javascript/hello-word/index.html'))).toBe(true);
+  expect(fs.existsSync(path.join(outputPath, 'javascript/hello-word/index.json'))).toBe(true);
+});
+
+test('integrated test render simemap.txt', async () => {
+  const renderCtrl = new RenderController(inputPath, outputPath, readConfigure(path.join(__dirname, '../mock-source')));
+  await renderCtrl.render();
+
   expect(fs.readFileSync(path.join(outputPath, 'sitemap.txt'), 'utf8')).toBe(
-`//test.starfish.org/javascript/hello-word/index.html
-//test.starfish.org/javascript/metor/index.html`
+    `//test.starfish.org/javascript/hello-word/index.html
+    //test.starfish.org/javascript/metor/index.html`
   );
 });
+
