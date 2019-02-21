c3b5314fc47037d92dbd38b79f8768f97c673072
fwchen <me@chenfangwei.com>
2019-02-21 16:16:22 +0800

    add parse test

--- src/parser/org-parser.test.ts
+++ src/parser/org-parser.test.ts
@@ -1,6 +1,14 @@
 import OrgParser from './org-parser';
 
-test('markdown parser check filename', () => {
+test('org mode parser check filename', () => {
+  const parser = new OrgParser();
+
+  expect(parser.check('/d/workspace/mock.org')).toEqual(true);
+  expect(parser.check('/d/workspace/mock.m')).toEqual(false);
+  expect(parser.check('/d/workspace/mock.mdx')).toEqual(false);
+});
+
+test('org mode parser check filename', () => {
   const parser = new OrgParser();
 
   expect(parser.check('/d/workspace/mock.org')).toEqual(true);
