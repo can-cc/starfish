743e5d19918ae22db7bb26f533bf12300ac585e2
fwchen <me@chenfangwei.com>
2019-02-21 16:15:26 +0800

    split functions

--- src/command/render.ts
+++ src/command/render.ts
@@ -1,36 +1,70 @@
 import * as ora from 'ora';
 import * as rimraf from 'rimraf';
-import { isDir, logCurrentTime } from '../lib/util';
+import { isDir } from '../lib/util';
 import { RenderController } from '../modules/render/render-controller';
 import { Command } from './command';
+import printer from '../util/printer';
 
 export default class RenderCommand implements Command {
   public name = 'render';
   public type = 'command';
+  private spinner;
 
-  public run(inputs: string[], flags: boolean[], blogConfigure: any) {
+  public run(inputs: string[], flags: boolean[], blogConfigure: BlogConfigure): void {
     const inputPath = inputs[0];
     if (!inputPath) {
       return console.error('Please spec blog path.');
     }
 
-    if (isDir(inputPath)) {
-      // TODO fix
-      const outputPath = 'build';
+    if (!isDir(inputPath)) {
+      // TODO console.log
+      return;
+    }
 
-      // TODO 做一个theme检查
-      rimraf(outputPath, () => {
-        const spinner = ora('Start render...').start();
+    const outputPath = 'build';
+
+      this.cleanOutPutAssets(outputPath).then(() => {
+
+        this.startSpin();
         try {
           const renderControl = new RenderController(inputPath, outputPath, blogConfigure);
           renderControl.render();
-          spinner.succeed('Render completion...');
+          this.stopSpinSuccess();
         } catch (error) {
           console.error(error);
-          spinner.fail('Build Fail...');
+          this.stopSpinFail();
         }
-        logCurrentTime();
+
+        this.logCurrentTime();
+
       });
-    }
+  }
+
+  private startSpin() {
+    this.spinner = ora('Start render...').start();
+  }
+
+  private stopSpinSuccess() {
+    this.spinner.succeed('Render completion...');
+  }
+
+  private stopSpinFail() {
+    this.spinner.fail('Build Fail...');
+  }
+
+  private cleanOutPutAssets(outputPath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      rimraf(outputPath, (error: Error) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve();
+      });
+    });
+  }
+
+  private logCurrentTime() {
+    let date = new Date();
+    printer.log(`Time: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
   }
 }
