64937ebc763f28b8d052c71c6499704066816c0d
fwchen <me@chenfangwei.com>
2019-02-26 15:30:49 +0800

    initi command opt

--- src/command/init.ts
+++ src/command/init.ts
@@ -1,7 +1,3 @@
-import * as ora from 'ora';
-import * as rimraf from 'rimraf';
-import { isDir, logCurrentTime } from '../lib/util';
-import { RenderController } from '../modules/render/render-controller';
 import { Command } from './command';
 
 export default class InitCommand implements Command {
