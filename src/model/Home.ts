59c718a40d9ebc1700ad47c01d814e4808988b46
fwchen <me@chenfangwei.com>
2019-02-26 15:31:05 +0800

    format

--- src/model/Home.ts
+++ src/model/Home.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import Category from './Category';
+import { Category } from './Category';
 import { RenderController } from '../modules/render/render-controller';
 
 export class BlogHome {
@@ -13,7 +13,6 @@ export class BlogHome {
   ) {}
 
   public render(): void {
-
     const indexData = {
       // ...this.controller.getBlogInformation(),
       // articles: R.take(10, allarticles).map(a => a.data),
