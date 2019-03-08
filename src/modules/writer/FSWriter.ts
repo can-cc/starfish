af8d7ad9ec6e7258358bd017c02ccc2e77fcb8b2
fwchen <me@chenfangwei.com>
2019-03-08 09:31:27 +0800

    implement writer func

--- src/modules/writer/FSWriter.ts
+++ src/modules/writer/FSWriter.ts
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import { Writer } from "./Writer";
 
-
 export class FSWriter implements Writer {
-    
+    public writeFileSync(filePath: string, content: string): void {
+      fs.writeFileSync(filePath, content);
+    }
 }
--- src/modules/writer/Writer.ts
+++ src/modules/writer/Writer.ts
@@ -1,4 +1,5 @@
 
 
 export interface Writer {
+    writeFileSync: (filePath: string, content: string) => void;
 }
