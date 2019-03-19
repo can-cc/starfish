6fdeb302cc707bcfdf127b34ef67f0506d2ecd73
fwchen <me@chenfangwei.com>
2019-03-19 11:09:32 +0800

    add function reader

--- src/modules/reader/FSReader.ts
+++ src/modules/reader/FSReader.ts
@@ -4,18 +4,26 @@ import path from 'path';
 import { Reader } from "./Reader";
 import { isDir } from '../../lib/util';
 
-// 
 export class FSReader implements Reader {
 
+    // TODO rename more common
     public readCategoryPaths(blogInputPath: string): string[] {
         return fs
             .readdirSync(blogInputPath)
             .filter(p => isDir(path.join(blogInputPath, p)));
     }
 
+    public readDirPaths(dirPath: string): string[] {
+        return fs.readdirSync(dirPath);
+    }
+
     public readYaml(filePath: string): string {
         return yaml.safeLoad(
             fs.readFileSync(filePath, 'utf8')
           );
+    }
+
+    public fileExist(filePath: string): boolean {
+        return fs.existsSync(filePath);
     }
 }
--- src/modules/reader/Reader.ts
+++ src/modules/reader/Reader.ts
@@ -3,4 +3,6 @@
 export interface Reader {
   readCategoryPaths(blogInputPath: string): string[];
   readYaml(filePath: string): any;
+  fileExist(filePath: string): boolean;
+  readDirPaths(dirPath: string): string[];
 }
